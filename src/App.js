e5f32bde7a6cb34706bc8236815eff3061a24168
Bryan Salicco <bryan.salicco@fedex.com>
2019-08-21 20:05:09 -0600

    trying to get NavBar to work - there was an issue with creating two components names AppBar ( one from material and one I was creating )

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from './pages/Home';
 import NoMatch from './pages/NoMatch';
-import AppBar from './components/AppBar';
+import NavBar from './components/NavBar';
 
 function App() {
   return(
     <Router>
         <div>
-            <AppBar/>
+            <NavBar/>
             <Switch>
                 <Route exact path="/" component={Home}/>
                 <Route component={NoMatch}/>
