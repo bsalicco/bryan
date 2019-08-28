aea43ad37342fff847227df430eb36096391b017
Bryan Salicco <bryan.salicco@fedex.com>
2019-08-27 18:10:32 -0600

    updated the home page to have a container layout with rows and columns fo the jumbo and the cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,28 @@
 import React from "react";
+import {Container, Row, Col, Button} from 'reactstrap';
+import Jumbo from '../components/Jumbo';
+import CardHome from '../components/CardHome';
 
-function Home() {
-    return (
-        <p>Hello Welcome</p>
-    );
+export default class Home extends React.Component {
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col><Jumbo/></Col>
+                </Row>
+                <Row>
+                    <Col><CardHome title="#WatsyBubby" text ="The Best Boy Pics!"
+                                   link="https://www.instagram.com/explore/tags/watsybubby/"
+                                   buttonTitle ="Watsy"/></Col>
+                    <Col><CardHome title="UCF Hockey" text ="UCF Hockey Alumni"
+                                   link="http://www.ucfhockey.com/"
+                                   buttonTitle ="UCF Hockey"/></Col>
+                    <Col><CardHome title="U of Denver Bootcamp" text ="Adjuct Bootcamp Professor"
+                                   buttonTitle="Explore"/></Col>
+                    <Col><CardHome title="Ikon" text ="Oh The Places You Board"
+                                   buttonTitle ="Ikon Map"/></Col>
+                </Row>
+            </Container>
+        );
+    }
 }
-
-export default Home;
